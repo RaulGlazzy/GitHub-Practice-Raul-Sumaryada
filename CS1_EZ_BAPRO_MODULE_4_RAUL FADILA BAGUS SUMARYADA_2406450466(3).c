b645f73632451849ccf5da0d93161899c023a28b
RaulGlazzy <postulatraul3.14@gmail.com>
2024-12-07 11:23:37 +0700

    Add files via upload

---
+++ CS1_EZ_BAPRO_MODULE_4_RAUL FADILA BAGUS SUMARYADA_2406450466(3).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+char ch1;
+char ch2;
+char ch3;
+char ch4;
+
+printf("=OPTIONS=\n");
+printf("Ryan\n");
+printf("Andrea\n");
+printf("Alsha\n");
+printf("David\n");
+
+printf("Enter a number.\n");
+
+return 0;
+}
